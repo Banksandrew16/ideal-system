40eb12c52b0ff493341ce95e18d680fc813dcfaa
Banksandrew16 <88170202+Banksandrew16@users.noreply.github.com>
2022-01-15 19:58:18 -0500

    update

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,5 @@ const userRoutes = require('./user-routes');
 
 // add prefix of `/thought` routes created in `thought-routes.js`
 router.use('/thought', thoughtRoutes);
-// add prefix of `/users` routes created in `user-routes.js`
 router.use('/users', userRoutes);
 module.exports = router;
